ec0eae7dad38c645da1005c99fc682c2461edcea
Viktor Uhryn <vik.ugrin@gmail.com>
2018-08-20 17:01:57 +0200

    add animations block

--- src/app/components/animations/animations.module.ts
+++ src/app/components/animations/animations.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AnimationsComponent } from './animations';
 import { CommonModule } from '@angular/common';
+import { RemoveConfirmationModule } from '../../ui-components/remove-confirmation/remove-confirmation.module';
 
 @NgModule({
   declarations: [
     AnimationsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    RemoveConfirmationModule
   ],
   exports: [
     AnimationsComponent
--- src/app/components/animations/animations.ts
+++ src/app/components/animations/animations.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { map, switchMap } from 'rxjs/operators';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { ActivatedRoute } from '@angular/router';
+import { RemoveConfirmationService } from '../../ui-components/remove-confirmation/remove-confirmation.service';
 
 @Component({
   selector: 'drwn-animations',
@@ -6,4 +11,60 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['animations.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AnimationsComponent {}
+export class AnimationsComponent {
+
+  @Output() change: EventEmitter<string> = new EventEmitter<string>();
+
+  index = -1;
+  id = null;
+
+  animations$;
+  animationsPath;
+
+  constructor(private store: AngularFirestore,
+              private auth: AngularFireAuth,
+              private route: ActivatedRoute,
+              private removeConfirmation: RemoveConfirmationService) {
+    this.animations$ = auth.user
+      .pipe(switchMap((user) => {
+        return route.params
+          .pipe(map((params) => {
+            return {drawingId: params.id, userId: user.uid};
+          }));
+      }))
+      .pipe(switchMap((ids: {drawingId: string, userId: string}) => {
+        this.animationsPath = `users/${ids.userId}/drawings/${ids.drawingId}/animations`;
+        return store.collection(this.animationsPath)
+          .snapshotChanges()
+          .pipe(map((actions) => {
+            return actions.map((item) => {
+              return {id: item.payload.doc.id, ...item.payload.doc.data()};
+            });
+          }));
+      }));
+  }
+
+  toggle(index, id) {
+    this.index = index;
+    this.id = id;
+    this.change.emit(this.id);
+  }
+
+  add(event) {
+    this.store.collection(this.animationsPath).add({
+      created: +new Date()
+    }).then();
+  }
+
+  remove(event, closeLink, id) {
+    this.removeConfirmation.open(closeLink, 'You are going to remove it forever. Are you sure?')
+      .after().subscribe((confirmation) => {
+      if (confirmation) {
+        this.store.doc(`${this.animationsPath}/${id}`).delete().then();
+      }
+    });
+
+    event.preventDefault();
+    event.stopPropagation();
+  }
+}
